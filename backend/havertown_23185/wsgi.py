ac6bc9ac27b9c5d7bff48126eb4b3ada5dbbc171
Crowdbotics <team@crowdbotics.com>
2020-12-04 01:02:47 +0000

    Update models

--- backend/havertown_23185/wsgi.py
+++ backend/havertown_23185/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'havertown_23185.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "havertown_23185.settings")
 
 application = get_wsgi_application()
